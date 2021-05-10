64cdd85611e9ac816bf9a54caaa042924f300201
shima47 <82750432+shima47@users.noreply.github.com>
2021-05-11 00:23:26 +0900

    Update PCRcalculatorPWAcustom.js

--- PCRcalculatorPWAcustom.js
+++ PCRcalculatorPWAcustom.js
@@ -1,50 +1,51 @@
+//name="oneVol"オブジェクト配列を取得
+const $oneVol = document.getElementsByName("oneVol");
+console
+
 //計算系
 const caluculate = (Num) => {
-	const oneVol = [7,1,1,0.075,0.8,9.075,9.875];
-	let resultVol = [7,1,1,0.075,0.8,9.075,9.875];
-	let i = 0;
+	//oneVolを配列に格納
+	let oneVol = [];
+	let oneVolIndex = 0;
+	while(oneVolIndex < $oneVol.length){
+		oneVol.push($oneVol[oneVolIndex].value);
+		oneVolIndex++;
+	}
+
 	//計算と格納
-	while(i < 7){
-		let a = oneVol[i] * Num;
+	let resultVol = [];
+	oneVolIndex = 0;
+	while(oneVolIndex < oneVol.length){
+		let a = oneVol[oneVolIndex] * Num;
 		let b = a * 1000;
 		let result = Math.round(b) / 1000;
 		
-		resultVol[i] = result;
-		i++;
+		resultVol.push(result);
+		oneVolIndex++;
 	};
 	
-	//項目オブジェクト配列を取得
-	const $col = document.getElementsByClassName("col");
-	
+	//id="multiple"オブジェクト配列を取得
+	const $multiple = document.getElementById("multiple");
 	//最上段右の項目を書き換え
-	$col[2].textContent = "x" + Num;
-	
-	//右列の項目を書き換え
-	i = 0;
-	let j = 5;
-	while(i < 5){
-		$col[j].textContent = resultVol[i] + "μL";
-		i++;
-		j = j + 3;
-	};
+	$multiple.textContent = "x" + Num;
 
-	//カスタムボタンを飛ばすため	
-	j = j + 3;
-	
-	while(i < 7){
-		$col[j].textContent = resultVol[i] + "μL";
-		i++;
-		j = j + 3;
+	//name="result"オブジェクト配列を取得
+	const $result = document.getElementsByName("result");
+	//右列の項目を書き換え
+	let resultIndex = 0;
+	while(resultIndex < $result.length){
+		$result[resultIndex].textContent = resultVol[resultIndex] + "μL";
+		resultIndex++;
 	};
 }
 
 //ボタンオブジェクト取得
-const $button = document.getElementsByTagName("button");
+const $button = document.getElementById("caluculate");
 //フォームオブジェクトを取得
 const $form = document.getElementById("form");
 
 //ボタンを押すと実行
-$button[0].addEventListener("click",() => {
+$button.addEventListener("click",() => {
 	//フォーム内の値を取得
 	const Num = $form.value;
 	caluculate(Num);
